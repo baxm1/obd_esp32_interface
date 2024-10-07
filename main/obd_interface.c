adfcd34109ffed5bcde8744b9102c55e7bc8b3ac
Brian <brp240@usask.ca>
2024-10-07 14:43:38 -0600

    initial commit

---
+++ main/obd_interface.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+
+
+
+
+void app_main(void)
+{
+
+    //xTaskCreatePinnedToCore(TaskFunction_t pxTaskCode, const char *const pcName, const uint32_t ulStackDepth, void *const pvParameters, UBaseType_t uxPriority, TaskHandle_t *const pxCreatedTask, const BaseType_t xCoreID)
+
+
+}
