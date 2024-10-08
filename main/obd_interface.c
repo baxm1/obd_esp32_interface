5fb0a646dd5fba598c28bfb1c6754d1d4de97449
Brian <brp240@usask.ca>
2024-10-08 17:11:00 -0600

    add simple task with print loop

--- main/obd_interface.c
+++ main/obd_interface.c
@@ -2,13 +2,36 @@
 
 #include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+
+#define STACKDEPTH_BYTES 2*1024
 
 
+TaskHandle_t obdTask = NULL;    // Task handler
+static const char* TAG = "obdModule";
+
+void obdtask(void *param){
+    for(;;){
+        ESP_LOGI(TAG, "obdtask");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    return;
+}
 
 void app_main(void)
-{
+{   
 
-    //xTaskCreatePinnedToCore(TaskFunction_t pxTaskCode, const char *const pcName, const uint32_t ulStackDepth, void *const pvParameters, UBaseType_t uxPriority, TaskHandle_t *const pxCreatedTask, const BaseType_t xCoreID)
+    xTaskCreatePinnedToCore(
+        &obdtask,                //Function to implement the task 
+        "obdtask",              //Name of the task
+        STACKDEPTH_BYTES,       //Stack size in words 
+        NULL,                   //Task input parameter 
+        configMAX_PRIORITIES-3, //Priority of the task 
+        &obdTask,                   //Task handle.
+        tskNO_AFFINITY);        //Core where the task should run 
 
 
 }
