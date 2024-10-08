c4184ac2e7cc1d26af5b3373147d1c4954af2cda
Brian <brp240@usask.ca>
2024-10-08 17:46:34 -0600

    added toggling of i/o pin

--- main/obd_interface.c
+++ main/obd_interface.c
@@ -6,18 +6,29 @@
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "esp_log.h"
+#include "driver/gpio.h" // want to do this without this header file at some point
 
 #define STACKDEPTH_BYTES 2*1024
 
+#define K_LINE_PIN 5
 
 TaskHandle_t obdTask = NULL;    // Task handler
-static const char* TAG = "obdModule";
 
 void obdtask(void *param){
+    char *taskName = pcTaskGetName(NULL); // get the name of this task
+
+    // configure GPIO pins
+    gpio_reset_pin(K_LINE_PIN);
+    gpio_set_direction(K_LINE_PIN, GPIO_MODE_OUTPUT);
+
     for(;;){
-        ESP_LOGI(TAG, "obdtask");
+        ESP_LOGI(taskName, "obdtask");
+
+        gpio_set_level(K_LINE_PIN, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+        gpio_set_level(K_LINE_PIN, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
     return;
 }
 
@@ -32,6 +43,5 @@ void app_main(void)
         configMAX_PRIORITIES-3, //Priority of the task 
         &obdTask,                   //Task handle.
         tskNO_AFFINITY);        //Core where the task should run 
-
 
 }
